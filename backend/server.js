0d17aedabad76f1b65313fe68f6831ca69333416
KelvinFerreiras <kferrei3@u.rochester.edu>
2019-09-20 21:39:15 -0500

    Express set up for API

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,17 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const PORT = 4000;
+const emailing = require("./routes/emailing");
+
+
 app.use(cors());
 app.use(bodyParser.json());
+app.use("/api/email", emailing);
+
+
+
 app.listen(PORT, function() {
     console.log("Server is running on Port: " + PORT);
 });
+
+
