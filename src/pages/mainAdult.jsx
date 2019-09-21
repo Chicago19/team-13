ef2bb5be2fb6d9e1182ac6c0152aaa2713e78152
Claire Su <suxiao10claire@gmail.com>
2019-09-21 00:14:12 -0400

    first commit?

---
+++ src/pages/mainAdult.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import ccaclogo from './images/ccaclogo.jpg';
+class CCACinfo extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Chicago Children's Advocacy Center</h1>
+                <img src ={ccaclogo} />
+                <p> description</p>
+            </div>
+        );
+    }
+}
+
+class resourcesButton extends React.Component{
+    render(){
+        return(
+            <button >Resources</button>
+        );
+    }
+}
+
+const MainAdultPage = () => {
+	
+	return (
+        <div className = "mainAdultPage">
+            <div >
+                <h1>Adult Homepage</h1>
+            </div>
+            <CCACinfo/>
+            <resourcesButton/>
+        </div>
+	);
+}
+
+
+export default MainAdultPage;
