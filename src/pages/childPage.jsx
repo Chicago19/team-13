057e27d17911fcfb36ef3feef36095ecc3526bae
Yixin Xiao <yixinx@umich.edu>
2019-09-21 08:47:05 -0400

    edited adultPage

--- src/pages/childPage.jsx
+++ src/pages/childPage.jsx
@@ -1,23 +1,28 @@
 import React from 'react';
 import { Row, Col, Container, Button, ButtonToolbar } from 'react-bootstrap';
+import {mahjong} from './images/mahjong.jpg';
+import { thisExpression } from '@babel/types';
 
-const renderButton = (props) =>{
-    return(
-        <Button>props</Button>
-    )
-}
+//https://cdn.htmlgames.com/DailyCrossword/
+//https://cdn.htmlgames.com/HugeSpiderSolitaire/
+//https://cdn.htmlgames.com/AnimalMahjong/
+
+
 
 export default class childPage extends React.Component{
+
     render(){
         return(
             <Container>
                 <Row>
-                    <ButtonToolbar className>
-                        <Button variant="primary">Yeet</Button>
-                        <Button variant="secondary">Yote</Button>
-                        <Button variant="success">Yought</Button>
-                    </ButtonToolbar>
+                    {/* <ButtonToolbar className> */}
+                        <a href="/games"><Button variant="primary" >Fruit Cubes</Button></a>
+                        <a href="/games1"><Button variant="secondary" >Daily Crossword</Button></a>
+                        <a href="/games2"><Button variant="success">Huge Spider Solitaire</Button></a>
+                        <a href="/games3"><Button variant="warning">Animal Mahjong</Button></a>
+                    {/* </ButtonToolbar> */}
                 </Row>
+                <embed width="800" height="600" src="https://cdn.htmlgames.com/FruitCubes/" ></embed>
             </Container>
         )
     }
