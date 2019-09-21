b3162bc00309f05cd857ef4469e0b0502b3e620b
Kevin <kevin.ko@berkeley.edu>
2019-09-21 07:09:35 -0700

    navbar for resources

--- src/pages/surveys.jsx
+++ src/pages/surveys.jsx
@@ -1,26 +1,27 @@
 import React from 'react';
 import BackButton from '../components/BackButton.jsx'
+import {renderNavBar} from '../global_components/header.jsx';
 
 import './css/surveys.css';
 
 class SurveyFrame extends React.Component{
     render(){
         return(
+          <>
+          <div> {renderNavBar("Surveys")}</div>
             <div>
                 <iframe src ="https://qtrial2019q3az1.az1.qualtrics.com/jfe/form/SV_9XgDI55w10vRKLP" height="800px" width="600px"></iframe>
-            
-            </div>
+
+            </div></>
         );
     }
 }
 
 const Surveys = () => {
-	
+
 	return (
         <div className = "surveys">
-            <div >
-                <h1 className="element">Surveys</h1>
-            </div>
+
             <SurveyFrame className="element" />
             <BackButton className="element" urlRoute="/claireslittyreactpage"/>
         </div>
