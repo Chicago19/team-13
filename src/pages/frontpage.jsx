273deb66ceedceb750a82834ac619a82f20d9a97
Yixin Xiao <yixinx@umich.edu>
2019-09-21 04:56:24 -0400

    added logo to home page, started changing font

--- src/pages/frontpage.jsx
+++ src/pages/frontpage.jsx
@@ -6,6 +6,7 @@ import './css/frontpage.css';
 //images
 import Parent from '../uploads/parents.jpg';
 import Children from '../uploads/children.jpg';
+import logo from './images/ccaclogo.jpg';
 class FrontPage extends React.Component{
   render(){
     return(
@@ -16,6 +17,7 @@ class FrontPage extends React.Component{
               <a href="/claireslittyreactpage">
                 <Image src={Parent} className="max-width filter"/>
                 <h1 className="center color-green">Parent</h1>
+                <Image src={logo} className="upper-left" roundedCircle/>
               </a>
             </Col>
             <Col className="no-padding max-width">
