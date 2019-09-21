69741a978e9a4fd2b5aa3c40fbdd97e4bb27569c
Claire Su <suxiao10claire@gmail.com>
2019-09-21 08:12:32 -0400

    ahhh4games

--- src/pages/childPage.jsx
+++ src/pages/childPage.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import { Row, Col, Container, Button, ButtonToolbar } from 'react-bootstrap';
-import {mahjong} from './images/animalmahjong.jpg';
-const renderButton = (props) =>{
-    return(
-        <Button>props</Button>
-    )
-}
+import {mahjong} from './images/mahjong.jpg';
+import { thisExpression } from '@babel/types';
+
+//https://cdn.htmlgames.com/DailyCrossword/
+//https://cdn.htmlgames.com/HugeSpiderSolitaire/
+//https://cdn.htmlgames.com/AnimalMahjong/
+
+
 
 export default class childPage extends React.Component{
+    
     render(){
         return(
             <Container>
                 <Row>
                     {/* <ButtonToolbar className> */}
-                        <input type ="image" src= {mahjong}/>
-                        <Button variant="secondary">Yote</Button>
+                        <Button variant = "primary" >Yeet</Button>
+                        <Button variant="secondary" >Yote</Button>
                         <Button variant="success">Yought</Button>
                     {/* </ButtonToolbar> */}
                 </Row>
