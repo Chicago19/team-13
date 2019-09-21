fc581653386ac5a3f43d44a7968f2dcbe2070d61
KelvinFerreiras <kferrei3@u.rochester.edu>
2019-09-21 10:56:02 -0500

    update to mailing server

--- backend/routes/emailing.js
+++ backend/routes/emailing.js
@@ -4,27 +4,52 @@ const nodemailer = require('nodemailer');
 
 
 let transport = nodemailer.createTransport({
-    host: 'smtp.mailtrap.io',
-    port: 2525,
+    host: 'smtp.googlemail.com', // Gmail Host
+    port: 465, // Port
+    secure: true, // this is true as port is 465
     auth: {
-       user: '52ff0094c3f2f8',
-       pass: 'ec30782c6f917b'
+       user: 'g13.cfg.test',
+       pass: 'notasecurepassword'
     }
 });
 
 
 function makeEmail(resources){
 
-    var body="Hello,<br><br> Thank you for requesting a personalized resource and referral guide. Here are the items you requested:<br><br>"
+    var body="Hello,<br><br> Here are the resources you requested:<br><br>"
     resources.forEach(function (resource) {
         body += "<strong>" +resource["title"]+"</strong><br>"
         body += "&emsp; - " +resource["description"]+"<br>"
         body += "&emsp; - <a href=\"" +resource["link"]+ "\">Learn More</a><br><br>"
     });
 
-    body+="Best regards,<br><br>Chicago Children's Advocacy Center<br> 1240 S. Damen Ave.<br> Chicago, IL 60608<br> (312) 492-3700<br>"
+    body+="Best regards,<br><br>Chicago Children's Advocacy Center<br>"
     return body;
+
+
+    // var body="Hello,<br><br> Thank you for requesting a personalized resource and referral guide. Here are the items you requested:<br><br>"
+    // resources.forEach(function (resource) {
+    //     body += "<strong>" +resource["title"]+"</strong><br>"
+    //     body += "&emsp; - " +resource["description"]+"<br>"
+    //     body += "&emsp; - <a href=\"" +resource["link"]+ "\">Learn More</a><br><br>"
+    // });
+
+    // body+="Best regards,<br><br>Chicago Children's Advocacy Center<br> 1240 S. Damen Ave.<br> Chicago, IL 60608<br> (312) 492-3700<br>"
+    // return body;
 }
+
+
+
+// Sample request body input:
+
+// {
+//     "email": "EMAIL@HERE.COM",
+//     "resources": [
+//         {"title":"title here", "description":"description here", "link":"link.com"},
+//         ... ,
+//     	{"title":"title here", "description":"description here", "link":"link.com"}
+//     	]
+// }
 
 router.post("/sendEmail", (req, res) => {
     let req_obj = JSON.parse(JSON.stringify(req.body));
@@ -32,7 +57,7 @@ router.post("/sendEmail", (req, res) => {
     let recipient = req_obj.email;
 
     const message = {
-        from: 'test@test.test', // Sender address
+        from: 'Code for Good Test<g13.cfg.test@gmail.com>', // Sender address
         to: recipient,         // List of recipients
         subject: 'Resources from Chicago Children Advocacy Center', // Subject line
         html: body // Plain text body
@@ -47,44 +72,7 @@ router.post("/sendEmail", (req, res) => {
 
   });
 
-// router.get("/sendEmail", (req, res) => {
-//     let m = makeEmail(resources)
-//     const message = {
-//         from: 'test@test.test', // Sender address
-//         to: 'test@test.tester',         // List of recipients
-//         subject: 'Testing this test', // Subject line
-//         text: m // Plain text body
-//     };
-//     transport.sendMail(message, function(err, info) {
-//         if (err) {
-//             res.send(err)
-//         } else {
-//             res.send(info)
-//         }
-//     });
 
-//   });
-
-
-// module.exports = router;
-
-// router.get("/test", (req, res) => {
-
-//     const message = {
-//         from: 'test@test.test', // Sender address
-//         to: 'test@test.tester',         // List of recipients
-//         subject: 'Testing this test', // Subject line
-//         text: 'Happy Testing!!!' // Plain text body
-//     };
-//     transport.sendMail(message, function(err, info) {
-//         if (err) {
-//             res.send(err)
-//         } else {
-//             res.send(info)
-//         }
-//     });
-
-//   });
 
 
 module.exports = router;
